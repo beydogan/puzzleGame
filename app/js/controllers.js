0b22f0e3b9e0504c49bfa4b1641dcdac267b23a9
Mehmet Beydogan <mbbrain@gmail.com>
2014-04-08 12:46:55 +0300

    adds grid and gridCell directives and templates

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,6 +4,8 @@
 
 angular.module('puzzleApp.controllers', [])
   .controller('HomeCtrl', ['$scope', function($scope) {
-
+    $scope.x = 15;
+    $scope.y = 15;
+    $scope.a = 50;
   }])
  
--- app/js/directives.js
+++ app/js/directives.js
@@ -3,4 +3,36 @@
 /* Directives */
 
 
-angular.module('puzzleApp.directives', [])
+angular.module('puzzleApp.directives', []).
+  directive('grid', function() {
+    return {
+    restrict: 'A',
+    templateUrl: 'partials/grid.html',
+    link: function (scope, elem, attrs) {
+      scope.rows = [
+        [1,2,3,4],
+        [5,6,7,8]
+      ]
+      
+      scope.size = 5;
+      console.log(attrs);
+    }
+  
+  
+  }
+  }).directive('gridCell', function() {
+    return {
+    restrict: 'A',
+    template: '<td>{{number}}</td>',
+    link: function (scope, elem, attrs) {
+      scope.number = attrs.gridCellNumber;
+      elem.css("border", "1px solid red");
+      elem.css("width", attrs.gridCellSize + "px");
+      elem.css("height", attrs.gridCellSize + "px");
+      console.log(attrs);
+    }
+  
+  
+  }
+  });
+
